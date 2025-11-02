afafe4dfe7b62d0118243bbb497e4c6e80dc79bb
PreyeO <omusukup@yahoo.com>
2025-11-02 11:16:23 +0100

    fix params error

--- app/(dashboard)/admin/directory/[id]/page.tsx
+++ app/(dashboard)/admin/directory/[id]/page.tsx
@@ -1,11 +1,13 @@
 import UserProfileView from "@/components/dashboard-screens/admins/UserProfileView";
 
+// Correct type for App Router dynamic route params
 interface UserProfilePageProps {
   params: {
     id: string;
   };
 }
 
+// No need to extend PageProps
 export default function UserProfilePage({ params }: UserProfilePageProps) {
   return <UserProfileView id={params.id} />;
 }
