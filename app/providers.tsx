9ae9f5342db576a7ae545cbec07a974587c92ed8
PreyeO <omusukup@yahoo.com>
2025-11-05 11:28:17 +0100

    remove themeprovider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { ThemeProvider } from "@/components/layout/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
 import { Toaster } from "sonner";
@@ -10,19 +9,12 @@ export default function ReactQueryProvider({
 }: {
   children: ReactNode;
 }) {
-  // ✅ prevent QueryClient from being re-created on every render
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <ThemeProvider
-        attribute="class"
-        defaultTheme="light"
-        enableSystem
-        disableTransitionOnChange
-      > */}
       {children}
-      {/* </ThemeProvider> */}
+
       <Toaster />
     </QueryClientProvider>
   );
